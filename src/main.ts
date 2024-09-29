c5520e2c3b26bbabeaef1c22da1bd6f1cc454892
Austin Merrick <austinmerrick@onsclom.net>
2024-09-28 23:12:06 -0700

    Migrate to vite

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import { init as initDynPages } from './dyn/DynPages';
+import { init as initCanvasBg } from './js/canvasBg';
+import { init as initGlassStack } from './js/glassStack';
+import { init as initMicroCarousel } from './js/microCarousel';
+
+initDynPages();
+initCanvasBg();
+initGlassStack();
+initMicroCarousel();
